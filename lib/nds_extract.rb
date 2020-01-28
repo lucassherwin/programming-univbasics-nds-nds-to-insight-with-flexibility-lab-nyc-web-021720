1c90de1e77939a7576d15e46dc6cfa751b45bd0b
Lucas Sherwin <lksherwin@gmail.com>
2020-01-28 20:30:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -52,7 +52,7 @@ def movies_with_director_key(name, movies_collection)
   movies = []
   index = 0
   while index < movies_collection.length do
-    movies[index] = movies_collection[index]
+    movies[index] = movie_with_director_name(name, movies_collection[index])
 
 
     index += 1
